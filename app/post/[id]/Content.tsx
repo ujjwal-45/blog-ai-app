469ee18d4dc5ef121b69e774503eb804926eb1ae
Ujjwal <ujjwalchoudhary065@gmail.com>
2023-06-10 16:13:22 +0530

    new changes

--- app/post/[id]/Content.tsx
+++ app/post/[id]/Content.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+ 'use client'
+import React, { useState} from 'react'
+import { FormattedPost } from '@/app/types'
 
-type Props = {}
+type Props = {
+    post: FormattedPost;
+};
 
-const Content = (props: Props) => {
+const Content = ({post}: Props) => {
+    const [isEditable, setisEditable] = useState<boolean>(false);
+
+    const [title, setTitle] = useState<string>(post.title);
+    const [titleError, setTitleError] = useState<string>("");
+ 
+     const [content, setContent] = useState<string>(post.content);
+     const [contentError, setContentError] = useState<string>("");
+
   return (
-    <div>Content</div>
+    <div>
+        
+        <h4 >
+            {`Home > ${post.category} > ${post.title}`}
+        </h4>
+    </div>
   )
 }
 
